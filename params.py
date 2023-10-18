4ada1be6b03f5248e4844ca6d8b3bbb2e6f91758
ann-creator <58597486+ann-creator@users.noreply.github.com>
2023-10-19 01:04:52 +0300

    Add files via upload

---
+++ params.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+
+class GeometryParams:
+    tvel_fuel_radius = 0.757 / 2
+    tvel_global_radius = 0.91 / 2
+    tvel_heigh = 350.0
+
+    orientation = 'x'
+
+    n_tvel_rows = 11
+    tvel_step = 1.275
+
+    tube_outer_radius = 1.26 / 2
+    tube_inner_radius = 1.26 / 2 - 0.085
+
+    absorber_rod_inner_radius = tvel_fuel_radius
+    absorber_rod_outer_radius = tvel_global_radius
+
+    rod_inserted = False
+    TVS_edge_length=14.025
